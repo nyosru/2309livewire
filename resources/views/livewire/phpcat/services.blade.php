27745005ff76ae2fb89ff8e9e04db0156a1d2a7e
Сергей <nyos@rambler.ru>
2023-09-16 13:48:46 +0500

    25

--- resources/views/livewire/phpcat/services.blade.php
+++ resources/views/livewire/phpcat/services.blade.php
@@ -5,7 +5,10 @@
     {{-- <div class="gap-2 columns-2xs columns-3md columns-5"> --}}
     {{-- <div class="flex flex-row:md gap-5"> --}}
     {{-- <div class="flex flex-wrap gap-5"> --}}
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 grid-auto-rows: auto; py-5">
+    <div
+        class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 grid-auto-rows: auto; py-5"
+{{--        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 grid-auto-rows: auto; py-5"--}}
+    >
         {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
         {{-- news --}}
         {{-- {{ $data }} --}}
@@ -14,9 +17,14 @@
                 {{-- {{ print_r($post) }} --}}
                 <h2 class="bg-green-200">{{ $i->title }}</h2>
                 {{-- <p>{{ $post->photo }}</p> --}}
-                <p><a href="{{ $i->link }}" target="_blank" >{{ $i->link_title }}</a></p>
+                <p><a class="text-blue-500" href="{{ $i->link }}" target="_blank" >{{ $i->link_title }}</a></p>
+                <img src="{{ $i->img_url }}" />
                 <p>{{ $i->opis }}</p>
                 <Br />
+
+                <div style="max-height: 175px; overflow: auto; font-size: 12px; border: 1px solid green; padding: 3px;">
+                {{ str_replace('"','" ',json_encode($i,true)) }}
+                </div>
             </div>
         @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,18 @@ use Illuminate\Support\Facades\Route;
 $d = function () {
     // Route::get('/', function () { return view('phpcat.index'); });
     Route::get('/', News::class)->name('index');
-    Route::get('news', News::class)->name('news');
-    Route::get('torrent', News::class)->name('torrent');
+//    Route::get('news', News::class)->name('news');
+//    Route::get('torrent', News::class)->name('torrent');
     Route::get('services', News::class)->name('services');
-    Route::get('money', News::class)->name('money');
+//    Route::get('money', News::class)->name('money');
     // Route::get('{.*}', News::class)->name('other');
     //    Route::fallback(function () { return redirect('/'); });
 };
-Route::group([
-    'as' => 'phpcat.',
-    'domain' => 'phpcat.local'
-], $d);
 //Route::group([
 //    'as' => 'phpcat.',
-//    'domain' => 'livewire.php-cat.com'
+//    'domain' => 'phpcat.local'
 //], $d);
+Route::group([
+    'as' => 'phpcat.',
+    'domain' => 'livewire.php-cat.com'
+], $d);
