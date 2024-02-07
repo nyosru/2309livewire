dbd2893c9cc0e44b64975c9f5c70c509f3c638e0
Sergey <1@php-cat.com>
2024-02-08 03:27:08 +0500

    52

--- routes/web.zem.php
+++ routes/web.zem.php
@@ -29,12 +29,12 @@ Route::group([
     'domain' => (env('APP_ENV', 'x') == 'local') ? 'zem.local' : 'приватизациягаража.рф'
 ], $d);
 
-//XN--80AAAAAHJ0AEHCC8FOJZ5E1I.XN--P1AI ( IDN = ПРИВАТИЗАЦИЯГАРАЖА.РФ ),
-Route::group([
-    'as' => 'zem.',
-//    'domain' => (env('APP_ENV', 'x') == 'local') ? 'zem.local' : 'земельныйкадастр.рф'
-    'domain' => (env('APP_ENV', 'x') == 'local') ? 'zem.local' : 'xn--80aalcakqihin5bmo2koa.xn--p1ai'
-], $d);
+////XN--80AAAAAHJ0AEHCC8FOJZ5E1I.XN--P1AI ( IDN = ПРИВАТИЗАЦИЯГАРАЖА.РФ ),
+//Route::group([
+//    'as' => 'zem.',
+////    'domain' => (env('APP_ENV', 'x') == 'local') ? 'zem.local' : 'земельныйкадастр.рф'
+//    'domain' => (env('APP_ENV', 'x') == 'local') ? 'zem.local' : 'xn--80aalcakqihin5bmo2koa.xn--p1ai'
+//], $d);
 
 //Route::group([
 //    'as' => 'zem.',
