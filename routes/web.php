6be527b0211e3619f66cdaf9a9f362621cbc9660
Sergey <1@php-cat.com>
2024-04-04 08:58:17 +0500

    97

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@
 
 
 Route::get('',function () {
+    return response( 'Привет', 200);
     return response()->json(['message' => 'Привет'], 200);
     return 'Привет буфет, ещё пару сек пожалуйста';
 });
