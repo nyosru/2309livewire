75ce107585c67b8632ed3441d9761c0a67efc7a9
Sergey <1@php-cat.com>
2024-04-04 21:39:32 +0500

    21

--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,36 @@ use Illuminate\Support\Facades\Route;
 //
 ////dd($_SERVER['HTTP_HOST']);
 //
-
-
-$ee = function() {
-    Route::get('/', function() {
-        dd([__FILE__, __LINE__, env('APP_ENV', 'x')]);
+Route::get('/', function() {
+    dd([__FILE__, __LINE__, env('APP_ENV', 'x')]);
 ////         return view('phpcat.index');
-    });
-};
+});
 
-
-
-Route::group(['domain' => 'управлятор.рф'], $ee);
-Route::group(['domain' => 'xn--80ae1ambgeod9j.xn--p1ai'], $ee);
-
-Route::group(['domain' => 'php-cat.com'], $ee);
-Route::group(['domain' => 'domainwaiter.com'], $ee);
-Route::group(['domain' => 'bu72.ru'], $ee);
-Route::group(['domain' => 'vk.files.php-cat.com'], $ee);
-
-Route::group(['domain' => 'продукты72.рф'], $ee);
-Route::group(['domain' => 'xn--72-jlcysfhth6f.xn--p1ai'], $ee);
-
-Route::group(['domain' => 'приватизациягаража.рф'], $ee);
-Route::group(['domain' => 'xn--80aaaaahj0aehcc8fojz5e1i.xn--p1ai'], $ee);
-
-Route::group(['domain' => 'земельныйкадастр.рф'], $ee);
-Route::group(['domain' => 'xn--80aalcakqihin5bmo2koa.xn--p1ai'], $ee);
+//$ee = function() {
+//    Route::get('/', function() {
+//        dd([__FILE__, __LINE__, env('APP_ENV', 'x')]);
+//////         return view('phpcat.index');
+//    });
+//};
+//
+//
+//
+//Route::group(['domain' => 'управлятор.рф'], $ee);
+//Route::group(['domain' => 'xn--80ae1ambgeod9j.xn--p1ai'], $ee);
+//
+//Route::group(['domain' => 'php-cat.com'], $ee);
+//Route::group(['domain' => 'domainwaiter.com'], $ee);
+//Route::group(['domain' => 'bu72.ru'], $ee);
+//Route::group(['domain' => 'vk.files.php-cat.com'], $ee);
+//
+//Route::group(['domain' => 'продукты72.рф'], $ee);
+//Route::group(['domain' => 'xn--72-jlcysfhth6f.xn--p1ai'], $ee);
+//
+//Route::group(['domain' => 'приватизациягаража.рф'], $ee);
+//Route::group(['domain' => 'xn--80aaaaahj0aehcc8fojz5e1i.xn--p1ai'], $ee);
+//
+//Route::group(['domain' => 'земельныйкадастр.рф'], $ee);
+//Route::group(['domain' => 'xn--80aalcakqihin5bmo2koa.xn--p1ai'], $ee);
 
 
 
