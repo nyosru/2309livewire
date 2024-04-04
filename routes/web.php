1e108e15fd1f856f2126b62048347b3281a6be11
Sergey <1@php-cat.com>
2024-04-04 08:56:50 +0500

    96

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
-//Route::get('{.*}',function () {
-//    return response( $_SERVER['HTTP_HOST'] ?? 'x' );
-////    return redirect('/');
-//});
-
-
-//dd($_SERVER['HTTP_HOST']);
-
-require('web.phpcat.php');
-require('web.domainwaiter.php');
-require('web.uprav.php');
-require('web.phpcat.files.php');
-require('web.ar.php');
-// земельный кадастр
-require('web.zem.php');
+////use Illuminate\Support\Facades\Route;
+////Route::get('{.*}',function () {
+////    return response( $_SERVER['HTTP_HOST'] ?? 'x' );
+//////    return redirect('/');
+////});
+//
+//
+////dd($_SERVER['HTTP_HOST']);
+//
+//require('web.phpcat.php');
+//require('web.domainwaiter.php');
+//require('web.uprav.php');
+//require('web.phpcat.files.php');
+//require('web.ar.php');
+//// земельный кадастр
+//require('web.zem.php');
 
 
 Route::get('',function () {
