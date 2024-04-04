f94e2b0cf74c6040b9cf6a6a7076730b97b84e1c
Sergey <1@php-cat.com>
2024-04-04 07:58:57 +0500

    90

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('{.*}',function () {
-    return response( $_SERVER['HTTP_HOST'] ?? 'x' );
-//    return redirect('/');
-});
+//Route::get('{.*}',function () {
+//    return response( $_SERVER['HTTP_HOST'] ?? 'x' );
+////    return redirect('/');
+//});
 
 
 //dd($_SERVER['HTTP_HOST']);
