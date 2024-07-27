169b58f73a3b9bdc6221a9ab370d8636ebdca4b8
Sergey <1@php-cat.com>
2024-07-27 13:03:04 +0500

    7-01

---
+++ app/Livewire/Skidki/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Skidki;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.skidki.index');
+    }
+}
