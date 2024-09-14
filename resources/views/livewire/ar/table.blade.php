61db40e8b2626113bfcb53746d82261822b45ce1
Sergey <1@php-cat.com>
2024-09-14 22:03:29 +0500

    7-07

--- resources/views/livewire/ar/table.blade.php
+++ resources/views/livewire/ar/table.blade.php
@@ -1,34 +1,40 @@
 <div>
 
-    <div class="text-white hover:text-gray-300" style="position: absolute; top: 10px; right: 30px;" >    {{ count($objects) }}</div>
-    {{-- {{ $objects }}--}}
+    <input type="password" wire:model.live="secret" style="position:absolute; top: 30px; right: 10px; border: 1px solid gray; width: 30px;"/>
 
-    <livewire:ar.object-add-form/>
+    @if($secret!=$envSecret)
+        Привет кукумба
+    @else
+        <div class="text-white hover:text-gray-300"
+             style="position: absolute; top: 10px; right: 30px;">    {{ count($objects) }}</div>
+        {{-- {{ $objects }}--}}
 
-    <style>
-        .pay-in-month {
-            /*.pay-in-month.pay-yes*/
-            display: block;
-        }
+        <livewire:ar.object-add-form/>
 
-        .pay-in-month.pay-yes {
-            xbackground-color: #70f276;
-        }
+        <style>
+            .pay-in-month {
+                /*.pay-in-month.pay-yes*/
+                display: block;
+            }
 
-        /*.pay-in-month.pay-no*/
-        .pay-in-month.pay-no {
-            xbackground-color: #f0f287;
-        }
+            .pay-in-month.pay-yes {
+                xbackground-color: #70f276;
+            }
 
-    </style>
+            /*.pay-in-month.pay-no*/
+            .pay-in-month.pay-no {
+                xbackground-color: #f0f287;
+            }
 
-    <div class="items md:mx-auto md:max-w-[40rem]">
-        @foreach( $objects as $o )
-            <livewire:ar.arObject
-                :object="$o"
-            />
-        @endforeach
-    </div>
-    {{--    @include('livewire.ar.add')--}}
+        </style>
 
+        <div class="items md:mx-auto md:max-w-[40rem]">
+            @foreach( $objects as $o )
+                <livewire:ar.arObject
+                    :object="$o"
+                />
+            @endforeach
+        </div>
+        {{--    @include('livewire.ar.add')--}}
+    @endif
 </div>
