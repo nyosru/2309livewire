b2524dbb77224712a33ce52c4e50b7c789c5ae01
Sergey <1@php-cat.com>
2024-09-14 10:31:18 +0500

    7-03

--- resources/views/livewire/ar/table.blade.php
+++ resources/views/livewire/ar/table.blade.php
@@ -3,7 +3,7 @@
     <div class="text-white hover:text-gray-300" style="position: absolute; top: 10px; right: 30px;" >    {{ count($objects) }}</div>
     {{-- {{ $objects }}--}}
 
-    <livewire:Ar.ObjectAddForm/>
+    <livewire:ar.object-add-form/>
 
     <style>
         .pay-in-month {
@@ -24,9 +24,9 @@
 
     <div class="items md:mx-auto md:max-w-[40rem]">
         @foreach( $objects as $o )
-            <livewire:Ar.arObject
-                :object="$o"
-            />
+{{--            <livewire:Ar.arObject--}}
+{{--                :object="$o"--}}
+{{--            />--}}
         @endforeach
     </div>
     {{--    @include('livewire.ar.add')--}}
