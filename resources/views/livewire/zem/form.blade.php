ac2f63b2d0753d9117179f51c082d881a7342ef9
Sergey <1@php-cat.com>
2024-09-21 06:45:55 +0500

    7-16

--- resources/views/livewire/zem/form.blade.php
+++ resources/views/livewire/zem/form.blade.php
@@ -21,7 +21,7 @@
                             class="relative flex h-12 w-full mx-auto items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:bg-white before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                         >
                   <span class="relative text-base font-semibold text-purple-600"
-                  >Начать!</span
+                  >Отправить заявку!</span
                   >
                         </a>
                     </div>
