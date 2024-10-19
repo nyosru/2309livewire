fdf3ef00c7b1cbcafa3a8d5d575ecd820680f6f5
Sergey <1@php-cat.com>
2024-10-19 23:47:31 +0500

    8-50

---
+++ database/migrations/2024_10_19_184650_change_published_at_column_type_in_st_news_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('st_news', function (Blueprint $table) {
+            // Изменение типа поля published_at с date на timestamp
+            $table->timestamp('published_at')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('st_news', function (Blueprint $table) {
+            // Возвращаем тип поля published_at обратно на date
+            $table->date('published_at')->change();
+        });
+    }
+};
