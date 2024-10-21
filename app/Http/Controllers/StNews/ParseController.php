95d679f5534946149a66495c1db6dab77582c7fc
Sergey <1@php-cat.com>
2024-10-21 11:05:15 +0500

    11

--- app/Http/Controllers/StNews/ParseController.php
+++ app/Http/Controllers/StNews/ParseController.php
@@ -803,10 +803,12 @@ class ParseController extends Controller
             foreach ($items as $i) {
                 $return['loaded_news'][] =
                 $data = $this->loadParsingNewsItem($i);
+                if (!empty($data['error'])) {
+                    $return['saved_news'][] = $data;
+                } else {
 //                dd($data);
-                $return['saved_news'][] = $this->saveParseNewsFullData($i, $data);
-
-                
+                    $return['saved_news'][] = $this->saveParseNewsFullData($i, $data);
+                }
             }
             return response()->json($return);
 
