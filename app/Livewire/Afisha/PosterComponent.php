7cfc8c3e198a6e447d8304597421672886631c14
Sergey <1@php-cat.com>
2024-11-01 23:39:04 +0500

    9-03

--- app/Livewire/Afisha/PosterComponent.php
+++ app/Livewire/Afisha/PosterComponent.php
@@ -23,8 +23,19 @@ class PosterComponent extends Component
                     $query->whereNotNull('end_date')
                         ->where('end_date', '>=', $currentDate);
                 });
-        })->get()->map(function($poster) {
+        })->orderBy('event_date')->orderBy('event_time')
+            ->get()
+            ->map(function($poster) {
             // Преобразование строковых дат в объекты Carbon
+            $dn = date('w', strtotime(Carbon::parse($poster->event_date)));
+            if( $dn == 0 ){ $poster->dn = 'Воскресенье'; }
+            elseif( $dn == 1 ){ $poster->dn = 'Понедельник'; }
+            elseif( $dn == 2 ){ $poster->dn = 'Вторник'; }
+            elseif( $dn == 3 ){ $poster->dn = 'Среда'; }
+            elseif( $dn == 4 ){ $poster->dn = 'Четверг'; }
+            elseif( $dn == 5 ){ $poster->dn = 'Пятница'; }
+            elseif( $dn == 6 ){ $poster->dn = 'Суббота'; }
+
             $poster->event_date = Carbon::parse($poster->event_date);
             if ($poster->end_date) {
                 $poster->end_date = Carbon::parse($poster->end_date);
