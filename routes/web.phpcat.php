7cc8cd3c22ef429a69f295ee7b682ed9a63c72e0
Sergey <1@php-cat.com>
2024-11-05 23:27:02 +0500

    9-12

--- routes/web.phpcat.php
+++ routes/web.phpcat.php
@@ -13,7 +13,7 @@ $d = function () {
 
 	Route::any('/f', \App\Livewire\Phpcat\VkFriends::class)->name('vk_friends');
 //	Route::any('/f/{code}', \App\Livewire\Phpcat\VkFriends::class)->name('vk_friends3');
-	Route::any('/callback', \App\Livewire\Phpcat\VkFriends::class)->name('vk_friends2');
+	Route::get('/callback', \App\Livewire\Phpcat\VkFriends::class)->name('vk_friends2');
 //	Route::get('/callback', function (\Illuminate\Http\Request $request) {
 //		return app(\App\Livewire\Phpcat\VkFriends::class)->getAccessToken($request->code);
 //	});
