469f4054b016a9619673c9ed2761a03e269f7545
Sergey <1@php-cat.com>
2025-03-11 04:44:29 +0500

    phpcatru+

--- resources/views/livewire/phpcatru/layouts/header.blade.php
+++ resources/views/livewire/phpcatru/layouts/header.blade.php
@@ -1,9 +1,15 @@
 <header class="flex flex-row shadow-xl">
     {{--    <div class="pt-[20%]">--}}
     <div class="px-[40px] py-[20px]">
-        <a href="/" >
-        <h1 class="text-[3rem] shadow-text">Сергей Бакланов<sup><span
-                    class="text-[1rem]">Домашняя страничка</span></sup></h1>
+        <a href="/">
+            <h1 class="text-[3rem] text-white font-bold" style="text-shadow: 5px 5px 10px #444;">
+                Сергей Бакланов
+                <sup>
+                <span class="text-[1rem]">
+                    Домашняя страничка
+                </span>
+                </sup>
+            </h1>
         </a>
     </div>
     {{--    <div>--}}
