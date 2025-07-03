7084dc2fd76fbd07bd8f8646b45b82d73002f51d
Сергей <1@php-cat.com>
2025-07-03 13:19:36 +0500

    250703++

--- resources/views/zaglushka/index.blade.php
+++ resources/views/zaglushka/index.blade.php
@@ -15,23 +15,24 @@
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css"/>
-    <script src="https://cdn.tailwindcss.com/3.3.0"></script>
-    <script>
-        tailwind.config = {
-            darkMode: 'class',
-            theme: {
-                fontFamily: {
-                    sans: ['Roboto', 'sans-serif'],
-                    body: ['Roboto', 'sans-serif'],
-                    mono: ['ui-monospace', 'monospace']
-                }
-            },
-            corePlugins: {
-                preflight: false
-            }
-        };
-    </script>
 
+{{--    <script src="https://cdn.tailwindcss.com/3.3.0"></script>--}}
+{{--    <script>--}}
+{{--        tailwind.config = {--}}
+{{--            darkMode: 'class',--}}
+{{--            theme: {--}}
+{{--                fontFamily: {--}}
+{{--                    sans: ['Roboto', 'sans-serif'],--}}
+{{--                    body: ['Roboto', 'sans-serif'],--}}
+{{--                    mono: ['ui-monospace', 'monospace']--}}
+{{--                }--}}
+{{--            },--}}
+{{--            corePlugins: {--}}
+{{--                preflight: false--}}
+{{--            }--}}
+{{--        };--}}
+{{--    </script>--}}
+    <link rel="stylesheet" href="/css/output.css?v=1" />
     @livewireStyles
 
     <!-- Facebook Meta Tags -->
@@ -160,7 +161,7 @@
             <A class="text-blue-800 underline" href="https://php-cat.com" target="_blank">php-cat.com</a><br/>
             и телеграм <A class="text-blue-800 underline" href="https://t.me/phpcatcom" target="_blank">@phpcatcom</a>
         </div>
-        
+
     </div>
 
 </main>
