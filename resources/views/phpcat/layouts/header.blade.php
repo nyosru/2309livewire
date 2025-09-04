e314e9efed7987aba9df8963ec4c8dfb6406650b
nyosru <nyos@rambler.ru>
2025-09-04 21:01:03 +0500

    12

--- resources/views/phpcat/layouts/header.blade.php
+++ resources/views/phpcat/layouts/header.blade.php
@@ -1,3 +1,9 @@
+
+
+@php
+    $currentDomain = request()->getHost();
+@endphp
+
 {{--<div class="text-center pb-3">--}}
 <a href="/" class="p-3 m-0 block text-center"><h1 style="font-size: 3rem;">
         <img src="/phpcat/cat.png" style="display:inline-block; max-height: 5rem;" />
