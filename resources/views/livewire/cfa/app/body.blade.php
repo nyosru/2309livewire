429c7b2f4e750a4d9879043f4c371df75cfa71be
nyosru <nyos@rambler.ru>
2025-09-18 20:19:18 +0500

    16

--- resources/views/livewire/cfa/app/body.blade.php
+++ resources/views/livewire/cfa/app/body.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <title>Центр Финансовой Аналитики</title>
     <link href="/css/output.css?v={{ filemtime(public_path('/css/output.css')) }}" rel="stylesheet">
