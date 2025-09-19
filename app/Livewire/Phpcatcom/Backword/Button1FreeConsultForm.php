910ee39b8ce584a691fa7da7c80e566211e5e3d8
nyosru <nyos@rambler.ru>
2025-09-19 13:46:02 +0500

    18 админка новостей

---
+++ app/Livewire/Phpcatcom/Backword/Button1FreeConsultForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Phpcatcom\Backword;
+
+use Livewire\Component;
+
+class Button1FreeConsultForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.phpcatcom.backword.button1-free-consult-form');
+    }
+}
---
+++ resources/views/livewire/phpcatcom/backword/button1-free-consult-form.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col md:flex-row">
+    <div>Бесплатная консультация</div>
+    <div></div>
+    <div></div>
+</div>
