9176f13841d22836da673262c08bec08f9497bd2
nyosru <nyos@rambler.ru>
2025-10-04 14:32:03 +0500

    49 ++ концепт мебель - макет превью для ссылок++

--- resources/views/livewire/concept-mebel/app/body.blade.php
+++ resources/views/livewire/concept-mebel/app/body.blade.php
@@ -132,20 +132,19 @@
 
 {{--<livewire:krostu.app.footer/>--}}
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104380256', 'ym');
 
-{{--<!-- Yandex.Metrika counter -->--}}
-{{--<script type="text/javascript">--}}
-{{--    (function(m,e,t,r,i,k,a){--}}
-{{--        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};--}}
-{{--        m[i].l=1*new Date();--}}
-{{--        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}--}}
-{{--        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)--}}
-{{--    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104224527', 'ym');--}}
-
-{{--    ym(104224527, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});--}}
-{{--</script>--}}
-{{--<noscript><div><img src="https://mc.yandex.ru/watch/104224527" style="position:absolute; left:-9999px;" alt="" /></div></noscript>--}}
-{{--<!-- /Yandex.Metrika counter -->--}}
+    ym(104380256, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/104380256" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 </body>
 @livewireScripts
