3d9eae4226a5b5445bb5ca158b59064ed1792906
nyosru <nyos@rambler.ru>
2025-10-04 13:59:18 +0500

    49 ++ концепт мебель - макет превью для ссылок++

--- resources/views/livewire/concept-mebel/index.blade.php
+++ resources/views/livewire/concept-mebel/index.blade.php
@@ -1,4 +1,25 @@
 <div class="flex justify-center items-center h-screen">
     <img src="/concept-mebel/concept-mebel.png" alt="Concept Mebel" class="max-h-screen max-w-full object-contain
     rounded-lg shadows-lg" />
+    <span class="fixed bottom-3 right-3">
+           <div class="flex flex-row items-center justify-center
+            text-sm
+            bg-[rgba(255,255,255,0.4)] px-2 rounded">
+                                <div>
+                                    <a href="https://php-cat.com"
+                                       class="whitespace-nowrap text-blue-600 hover:underline"
+                                       target="_blank">
+                                        <img src="/phpcat/cat.png" class="inline-block h-[3rem]"/>
+                                    </a>
+                                </div>
+                                <div class="text-center">
+                                    Создание сайта<br/>
+                                    <a href="https://php-cat.com"
+                                       class="whitespace-nowrap text-blue-600 hover:underline"
+                                       target="_blank">
+                                        php-cat.com
+                                    </a>
+                                </div>
+                            </div>
+    </span>
 </div>
