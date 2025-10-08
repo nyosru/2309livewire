3170c2c3049b5d90911b66aa28d25e120b87647f
nyosru <nyos@rambler.ru>
2025-10-09 02:45:21 +0500

    54 ++ добавил сервисы доп++

---
+++ app/Livewire/Phpcat/ServicesDop.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Phpcat;
+
+use Livewire\Component;
+
+class ServicesDop extends Component
+{
+    public function render()
+    {
+        return view('livewire.phpcat.services-dop');
+    }
+}
