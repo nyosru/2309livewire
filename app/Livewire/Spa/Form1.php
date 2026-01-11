285948b151915cbe258e6a8440cbbe9176b8f312
nyosru <nyos@rambler.ru>
2026-01-11 20:54:07 +0500

    63 ттт2-1

--- app/Livewire/Spa/Form1.php
+++ app/Livewire/Spa/Form1.php
@@ -11,11 +11,18 @@ class Form1 extends Component
     public $phone;
 
     public function sendMsg(){
+
+        $msg = 'Заказ '.PHP_EOL.'указали телефон: '.$this->phone;
+
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 2, config( 'custom.BOT_TELEGA_ORDER' ) );
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, config( 'custom.BOT_TELEGA_ORDER' ) );
-        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, env('BOT_TELEGA_ORDER') );
+        Msg::sendTelegramm($msg,null, 2, env('BOT_TELEGA_ORDER') );
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, env( 'BOT_TELEGA_ORDER' ) );
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, env( 'BOT_TELEGA_ORDER' ) );
+
+
+//        Msg::sendTelegramm($msg, 360209578, 2, env('TOKEN_WARN_TELEGA'));
+
         // Устанавливаем флаг в сессию
         session()->flash('message_ok', 'Сообщение отправлено');
     }
