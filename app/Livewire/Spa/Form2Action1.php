4e719a8ab199ca35120c5b55b4f2c06204a70bbe
nyosru <nyos@rambler.ru>
2026-01-12 03:56:54 +0500

    63 ттт72 форма 5

--- app/Livewire/Spa/Form2Action1.php
+++ app/Livewire/Spa/Form2Action1.php
@@ -27,13 +27,16 @@ class Form2Action1 extends Component
 
         $this->getTelegramToken();
         if(empty($this->telegram_bot_token)){
-            $this->telegram_bot_token = env('BOT_TELEGA_ORDER','5960307100:AAHshaEf6WXw4rKbDg-JCeAyOEsFoHqZmNA');
+            $this->telegram_bot_token = env('BOT_TELEGA_TOKEN_URALWEBINFO');
             $text .= PHP_EOL.'**не указан токен телеграм**';
         }
 
 //        Msg::sendTelegramm($text ,null, 2, $this->telegram_bot_token );
-        Msg::sendTelegramm($text ,null, 1, $this->telegram_bot_token );
+//        Msg::sendTelegramm($text ,null, 2, $this->telegram_bot_token );
+//        Msg::sendTelegramm($text ,null, 2, $this->telegram_bot_token );
+//        Msg::sendTelegramm($text ,360209578, null, $this->telegram_bot_token );
 //        Msg::sendTelegramm($text ,null, 1 );
+        Msg::sendTelegramm($text,null, 2, env('TOKEN_WARN_TELEGA') );
 //        Msg::sendTelegramm($text ,'360209578', 2, $this->telegram_bot_token );
 
 //        dd(__FILE__,__LINE__, $text, $this->telegram_bot_token ?? 'x' );
