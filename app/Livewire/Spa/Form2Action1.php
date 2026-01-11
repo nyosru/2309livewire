55e5e3fc5aaa1044b72d36b415e779cf74d508dd
nyosru <nyos@rambler.ru>
2026-01-12 03:58:59 +0500

    63 ттт72 форма 6

--- app/Livewire/Spa/Form2Action1.php
+++ app/Livewire/Spa/Form2Action1.php
@@ -13,30 +13,32 @@ class Form2Action1 extends Component
     public $telegram_bot_token_config;
     public $telegram_bot_token;
 
-    public function getTelegramToken(){
+    public function getTelegramToken()
+    {
 
-        if( !empty($this->telegram_bot_token_config))
-        $this->telegram_bot_token = config($this->telegram_bot_token_config);
+        if (!empty($this->telegram_bot_token_config))
+            $this->telegram_bot_token = config($this->telegram_bot_token_config);
 
     }
 
-    public function sendMsg(){
+    public function sendMsg()
+    {
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 2, config( 'custom.BOT_TELEGA_ORDER' ) );
 
-        $text = 'Заказ '.PHP_EOL.'указали телефон: '.$this->phone;
+        $text = 'Заказ ' . PHP_EOL . 'указали телефон: ' . $this->phone;
 
-        $this->getTelegramToken();
-        if(empty($this->telegram_bot_token)){
-            $this->telegram_bot_token = env('BOT_TELEGA_TOKEN_URALWEBINFO');
-            $text .= PHP_EOL.'**не указан токен телеграм**';
-        }
+//        $this->getTelegramToken();
+//        if (empty($this->telegram_bot_token)) {
+//            $this->telegram_bot_token = env('BOT_TELEGA_TOKEN_URALWEBINFO');
+//            $text .= PHP_EOL . '**не указан токен телеграм**';
+//        }
 
 //        Msg::sendTelegramm($text ,null, 2, $this->telegram_bot_token );
 //        Msg::sendTelegramm($text ,null, 2, $this->telegram_bot_token );
 //        Msg::sendTelegramm($text ,null, 2, $this->telegram_bot_token );
 //        Msg::sendTelegramm($text ,360209578, null, $this->telegram_bot_token );
 //        Msg::sendTelegramm($text ,null, 1 );
-        Msg::sendTelegramm($text,null, 2, env('TOKEN_WARN_TELEGA') );
+        Msg::sendTelegramm($text, null, 2, env('TOKEN_WARN_TELEGA'));
 //        Msg::sendTelegramm($text ,'360209578', 2, $this->telegram_bot_token );
 
 //        dd(__FILE__,__LINE__, $text, $this->telegram_bot_token ?? 'x' );
