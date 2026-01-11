f9126098db04fc8eae03991a11563d154cb945d2
nyosru <nyos@rambler.ru>
2026-01-12 01:31:32 +0500

    63 приватизация гаража 2

--- app/Livewire/Spa/Form1.php
+++ app/Livewire/Spa/Form1.php
@@ -17,7 +17,8 @@ class Form1 extends Component
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 2, config( 'custom.BOT_TELEGA_ORDER' ) );
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, config( 'custom.BOT_TELEGA_ORDER' ) );
 //        Msg::sendTelegramm($msg,null, 2, env('BOT_TELEGA_ORDER') );
-        Msg::sendTelegramm($msg,360209578, 2, env('TOKEN_WARN_TELEGA') );
+//        Msg::sendTelegramm($msg,360209578, 2, env('TOKEN_WARN_TELEGA') );
+        Msg::sendTelegramm($msg,null, 2, env('TOKEN_WARN_TELEGA') );
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, env( 'BOT_TELEGA_ORDER' ) );
 //        Msg::sendTelegramm('Заказ '.PHP_EOL.'указали телефон: '.$this->phone,null, 1, env( 'BOT_TELEGA_ORDER' ) );
 
