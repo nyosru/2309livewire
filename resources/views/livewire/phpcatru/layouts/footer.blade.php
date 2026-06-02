6aad4944f2476450e851a44158b5c79d5030106e
nyosru <nyos@rambler.ru>
2026-06-02 14:36:55 +0500

    ++ php-cat.ru

--- resources/views/livewire/phpcatru/layouts/footer.blade.php
+++ resources/views/livewire/phpcatru/layouts/footer.blade.php
@@ -7,8 +7,8 @@
     Все права защищены &copy; {{ date('Y') }} | Создание сайта: <a href="https://php-cat.com" target="_blank"
         class="underline hover:text-gray-700 transition">php-cat.com</a>
     <br/>
-    Сервис CRM / летопись для вдения бищнес и личных дел : <a href="https://процессмастер.рф" target="_blank"
-        class="underline hover:text-gray-700 transition">ПроцессМастер.рф</a>
+    <a href="https://процессмастер.рф" target="_blank"
+       class="underline hover:text-gray-700 transition">ПроцессМастер.рф</a> сервис CRM/летопись - для ведения бизнес и личных дел
 
     <style>
         @keyframes moveGradient {
