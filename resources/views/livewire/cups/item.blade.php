d4bc749edabb2e40b7a0cdb02613c6a73b868383
nyosru <nyos@rambler.ru>
2026-09-24 12:18:23 +0500

    ++ cups up

--- resources/views/livewire/cups/item.blade.php
+++ resources/views/livewire/cups/item.blade.php
@@ -7,14 +7,20 @@
             w-full"/>
         @endif
 
-        @if( !empty($i['opis']) )
-            <div class="px-6 pt-4">
-                <p class="text-sm text-gray-600">{{ $i['opis'] }}</p>
-            </div>
-        @endif
+{{--        @if( !empty($i['opis']) )--}}
+{{--            <div class="px-6 pt-4">--}}
+{{--                <p class="text-sm text-gray-600">{{ $i['opis'] }}</p>--}}
+{{--            </div>--}}
+{{--        @endif--}}
 
-        <div class="px-6 pt-6 pb-2">
-            <h5 class="font-bold text-lg mb-3">{{ $i['name'] }}</h5>
-        </div>
+{{--        <div class="px-6 pt-6 pb-2">--}}
+            <h5
+                style="text-shadow: 2px 0 3px rgba(255, 255, 255, 0.6), -2px 0 3px rgba(255, 255, 255, 0.9),  0 3px 3px rgba(255, 255, 255, 0.6), 0 -2px 3px rgba(255, 255, 255, 0.9);"
+                class="
+                shadow-lg
+                inline absolute left-[10px]
+                bottom-[0]
+                font-bold text-lg mb-3">{{ $i['name'] }}</h5>
+{{--        </div>--}}
     </div>
 </div>
